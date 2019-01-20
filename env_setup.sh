d8963b258feaf7e0a950f4c403636b12e0e6040c
Rupert Broad <rupert.broad@gmail.com>
2019-01-20 14:25:24 +0100

    start

---
+++ env_setup.sh
@@ -0,0 +1,7 @@
+pip3 install virtualenv
+virtualenv env --python=python3
+source env/bin/activate
+
+pip3 install flask connexion Flask-SQLAlchemy flask-marshmallow marshmallow-sqlalchemy marshmallow swagger-ui-bundle
+
+
