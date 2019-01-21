205b28dcf2e3bcd287d277e8e519a2fd2e12a7dc
Rupert Broad <rupert.broad@gmail.com>
2019-01-21 21:44:41 +0100

    .

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
 import os
 import connexion
-# import pymysql
+import pymysql
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Create the connexion application instance
-connex_app = connexion.App(__name__, specification_dir=basedir)
+# connex_app = connexion.App(__name__, specification_dir=basedir)
 
 # Get the underlying Flask app instance
-app = connex_app.app
+# app = connex_app.app
 
 # Build the Sqlite ULR for SqlAlchemy
 # sql_url = "sqlite:////" + os.path.join(basedir, "people.db")
-sql_url = 'mysql://flask:ComplexPassw0rd!@localhost/people'
+
+app = flask(__name__)
+
+sql_url = 'mysql+pymysql://flask:ComplexPassw0rd!@localhost:3306/people'
 
 # Configure the SqlAlchemy part of the app instance
 app.config["SQLALCHEMY_ECHO"] = True
@@ -26,3 +29,6 @@ db = SQLAlchemy(app)
 
 # Initialize Marshmallow
 ma = Marshmallow(app)
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:3306/{DB_NAME}'
