43c979ff403c6f2cd0e67544f162088e0f60b51d
Rupert Broad <rupert.broad@gmail.com>
2019-01-26 20:59:15 +0100

    .

--- env_setup.sh
+++ env_setup.sh
@@ -1,10 +1,9 @@
-apt ins
+# Python 3
+sudo -H pip3 install virtualenv
+sudo -H virtualenv env3 --python=python3
+source env3/bin/activate
 
-sudo pip install virtualenv
-sudo virtualenv env --python=python3
-source env/bin/activate
-
-sudo pip install flask \
+sudo -H pip install flask \
   connexion \
   Flask-SQLAlchemy \
   flask-marshmallow \
@@ -15,9 +14,27 @@ sudo pip install flask \
   Flask-SQLAlchemy \
   PyMySQL \
   PyYAML \
-  mysqlclient \
+  mysqlclient 
 
-sudo pip install --upgrade setuptools
+sudo -H pip3 install --upgrade setuptools
 
+# Python 2
+sudo -H pip install virtualenv
+sudo -H virtualenv env2 --python=python3
+source env2/bin/activate
 
+sudo -H pip install flask \
+  connexion \
+  Flask-SQLAlchemy \
+  flask-marshmallow \
+  marshmallow-sqlalchemy \
+  marshmallow \
+  swagger-ui-bundle \
+  Flask-MySQL \
+  Flask-SQLAlchemy \
+  PyMySQL \
+  PyYAML \
+  mysqlclient 
+
+sudo -H pip install --upgrade setuptools
 
