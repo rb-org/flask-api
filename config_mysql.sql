ca2597cf0e0ae0f4bebe2230058975bd12ab271d
Rupert Broad <rupert.broad@gmail.com>
2019-01-26 19:52:19 +0100

    .

--- config_mysql.sql
+++ config_mysql.sql
@@ -1,19 +1,11 @@
-CREATE USER 'flask3'@'%' IDENTIFIED BY 'ComplexPassw0rd!';
-CREATE USER 'flask3'@'localhost' IDENTIFIED BY 'ComplexPassw0rd!';
-USE people
-GRANT ALL ON people.* TO 'flask3'@'%';
-GRANT ALL ON people.* TO 'flask3'@'localhost';
+CREATE USER 'flask'@'%' IDENTIFIED BY 'ComplexPassw0rd!';
+CREATE USER 'flask'@'localhost' IDENTIFIED BY 'ComplexPassw0rd!';
 
 CREATE DATABASE people;
 USE people
-GRANT ALL ON people.* TO 'flask2'@'%'
+GRANT ALL ON people.* TO 'flask'@'%' IDENTIFIED BY 'ComplexPassw0rd!';
+GRANT ALL ON people.* TO 'flask'@'localhost' IDENTIFIED BY 'ComplexPassw0rd!';
 WITH GRANT OPTION;
-quit;
-
-CREATE DATABASE cars;
-USE cars
-GRANT ALL ON cars.* TO 'flask'@'%';
-quit;
 
 USE people
 
@@ -30,4 +22,4 @@ INSERT INTO person VALUES(2, 'Mercedes', 'Benx',NOW());
 INSERT INTO person VALUES(3, 'Mini', 'Cooper', NOW());
 COMMIT;
 
-uninstall plugin validate_password;
+-- uninstall plugin validate_password;
